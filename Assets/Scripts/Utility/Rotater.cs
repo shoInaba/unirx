91eb54db23ec46edb15742c31ae3b67aed6727d7
inaba.sho <inaba.sho@donuts.ne.jp>
2017-07-31 10:18:15 +0900

    LoadAnimation 準備

---
+++ Assets/Scripts/Utility/Rotater.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+
+public class Rotater : MonoBehaviour {
+    public Vector3 speed;
+
+    void Start(){
+        // OnComplete処理も必要
+        this.UpdateAsObservable()
+            .Subscribe(_ => transform.Rotate(speed * Time.deltaTime));
+    }
+}
